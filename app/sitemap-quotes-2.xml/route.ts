851c538c929e7dbda5832eb07e07f4d671701a62
Aaron <151970160+linuxaaron@users.noreply.github.com>
2026-09-07 00:05:31 +0200

    seo: make overflow quote sitemap future-proof

--- app/sitemap-quotes-2.xml/route.ts
+++ app/sitemap-quotes-2.xml/route.ts
@@ -2,7 +2,6 @@ import quotesData from "../../data/quotes";
 import { isSeoIndexable, PRIMARY_SITEMAP_QUOTE_LIMIT } from "../../lib/seo";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://mayalines.com";
-const END_INDEX = 59000;
 
 function escapeXml(value: string) {
   return value.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
@@ -11,7 +10,7 @@ function escapeXml(value: string) {
 export async function GET() {
   const quotes = quotesData
     .filter(isSeoIndexable)
-    .slice(PRIMARY_SITEMAP_QUOTE_LIMIT, END_INDEX);
+    .slice(PRIMARY_SITEMAP_QUOTE_LIMIT);
 
   const urls = quotes.map((quote) => `<url><loc>${escapeXml(`${siteUrl}/quotes/${quote.slug}`)}</loc><changefreq>monthly</changefreq><priority>0.6</priority></url>`).join("");
   const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
